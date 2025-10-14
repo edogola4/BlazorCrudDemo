345eb64fad10bf62325415f8c0a00ca534736d60
edogola4 <edogola4@gmail.com>
2025-10-14 11:22:42 +0300

    chore: Update web project configuration
    - Update Program.cs with service registrations
    - Configure appsettings.json with connection strings
    - Update project file with package references

--- BlazorCrudDemo.Web/Program.cs
+++ BlazorCrudDemo.Web/Program.cs
@@ -1,27 +1,49 @@
-using BlazorCrudDemo.Web.Components;
+using BlazorCrudDemo.Data.Contexts;
+using BlazorCrudDemo.Data.Interfaces;
+using BlazorCrudDemo.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Blazored.Toast;
+using Blazored.Modal;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+// Add services to the container
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+// Add Entity Framework
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add Repositories
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+// Add Blazored services
+builder.Services.AddBlazoredToast();
+builder.Services.AddBlazoredModal();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-app.UseAntiforgery();
+app.UseRouting();
 
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+// Ensure database is created
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.EnsureCreated();
+}
 
 app.Run();
