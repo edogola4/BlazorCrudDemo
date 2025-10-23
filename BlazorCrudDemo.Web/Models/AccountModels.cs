e8accbaf3331b0aa77d6a5197480fd46fb40baf5
edogola4 <edogola4@gmail.com>
2025-10-23 13:21:46 +0300

    Add AccountModels for authentication data transfer

---
+++ BlazorCrudDemo.Web/Models/AccountModels.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorCrudDemo.Web.Models
+{
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+
+        public string Token { get; set; } = string.Empty;
+    }
+}
