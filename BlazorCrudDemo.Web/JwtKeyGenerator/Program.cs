cb225ff3ddc0b0a0399c4141f24665cbb0eac390
edogola4 <edogola4@gmail.com>
2025-10-23 13:23:00 +0300

    Add JWT key generator utility for development

---
+++ BlazorCrudDemo.Web/JwtKeyGenerator/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace JwtKeyGenerator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("JWT Key Generator");
+            // Add your JWT key generation logic here
+        }
+    }
+}
